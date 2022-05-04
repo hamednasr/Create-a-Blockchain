7424428a4e5a5ba2d5ba8d9fc6bc647f8e1c5fd4
hamed <hmd.nasr@gmail.com>
2022-05-04 09:22:16 -0400

    mine the new blockchain

--- blockchain.py
+++ blockchain.py
@@ -66,5 +66,20 @@ app = Flask(__name__)
 
 blockchain = Blockchain()
 
+# mining the new Blockchain
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    responce = {'message':'Congratulations, you just mined a block!',
+                'index':block['index'],
+                'timestamp':block['timestamp'],
+                'proof':block['proof'],
+                'previous_hash':block['previous_hash']}
+    return jsonify(responce) , 200
 
 
